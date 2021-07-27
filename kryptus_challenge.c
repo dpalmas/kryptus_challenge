612e5ea8201d0416c47ac5fa566d89a04bd2e887
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-07-27 19:21:18 -0300

    Update kryptus_challenge.c

--- kryptus_challenge.c
+++ kryptus_challenge.c
@@ -8,106 +8,112 @@
  ============================================================================
  */
 
-include <stdio.h>
+include<stdio.h>
 
-typedef void (*funcaoImpressao)  (void *inf);
-typedef  int (*funcaoComparacao) (void *, void *);
+typedef void (*funcaoImpressao)(void *inf);
+typedef int (*funcaoComparacao)(void *, void *);
 
 struct Noh
 {
-    struct Noh *anterior;	
-    void *info;
-    struct Noh *proximo;
+  struct Noh *anterior;
+  void *info;
+  struct Noh *proximo;
 };
 
 struct DLista
 {
-   struct Noh *primeiro;
-   struct Noh *ultimo;
-   int tamanho;
-   funcaoComparacao fc;
+  struct Noh *primeiro;
+  struct Noh *ultimo;
+  int tamanho;
+  funcaoComparacao fc;
 };
 
-struct DLista* criarLista(funcaoComparacao pfc)
+struct DLista *criarLista(funcaoComparacao pfc)
 {
   struct DLista *descritor;
 
-  descritor = (struct DLista *) malloc(sizeof(struct DLista));
-  descritor->primeiro   = NULL;
-  descritor->ultimo     = NULL;
+  descritor = (struct DLista *)malloc(sizeof(struct DLista));
+  descritor->primeiro = NULL;
+  descritor->ultimo = NULL;
   descritor->tamanho = 0;
-  descritor->fc         = pfc;
+  descritor->fc = pfc;
 
   return descritor;
 }
 
 void adicionarInfo(struct DLista *lista, void *novaInfo)
 {
-   struct Noh *novo;
- 
-   novo = (struct Noh *) malloc(sizeof(struct Noh));
-   lista->tamanho++;
+  struct Noh *novo;
 
-   novo->info = novaInfo;
-   novo->proximo = NULL;
+  novo = (struct Noh *)malloc(sizeof(struct Noh));
+  lista->tamanho++;
 
-   if(lista->primeiro == NULL)
-   {
-      lista->primeiro = novo;
-   } else {
-      lista->ultimo->proximo = novo;
-   }
+  novo->info = novaInfo;
+  novo->proximo = NULL;
 
-   lista->ultimo = novo;
+  if (lista->primeiro == NULL)
+  {
+    lista->primeiro = novo;
+  }
+  else
+  {
+    lista->ultimo->proximo = novo;
+  }
+
+  lista->ultimo = novo;
 }
 
 int vaziaListaDinamica(struct DLista *lista)
 {
-   return(lista->tamanho == 0);
+  return (lista->tamanho == 0);
 }
 
 void removerInfo(struct DLista *lista, void *info)
 {
-   struct Noh *atual, *anterior;
+  struct Noh *atual, *anterior;
 
-   if (lista == NULL)
-       return;
+  if (lista == NULL)
+    return;
 
-   anterior   = NULL;
-   atual = lista->primeiro;
+  anterior = NULL;
+  atual = lista->primeiro;
 
-   while(atual != NULL)
-   {
-       if (lista->fc(atual->info, info) == 0)
-       {
-           if (atual == lista->primeiro)
-           {
-             lista->primeiro = atual->proximo;
-           } else if(atual == lista->ultimo) {
-              lista->ultimo = anterior;
-              anterior->proximo = NULL;
-           } else {
-               anterior->proximo = atual->proximo;
-           }
+  while (atual != NULL)
+  {
+    if (lista->fc(atual->info, info) == 0)
+    {
+      if (atual == lista->primeiro)
+      {
+        lista->primeiro = atual->proximo;
+      }
+      else if (atual == lista->ultimo)
+      {
+        lista->ultimo = anterior;
+        anterior->proximo = NULL;
+      }
+      else
+      {
+        anterior->proximo = atual->proximo;
+      }
 
-           lista->tamanho--;
-           free(atual);
-           break;
-       }
-       anterior = atual;
-       atual = atual->proximo;
-   }
+      lista->tamanho--;
+      free(atual);
+      break;
+    }
+    anterior = atual;
+    atual = atual->proximo;
+  }
 }
 
 int insere_lista_inicio(struct DLista *lista, void *novaInfo)
 {
   struct Noh *novo;
 
-  novo = (struct Noh *) malloc(sizeof(struct Noh));
+  novo = (struct Noh *)malloc(sizeof(struct Noh));
   if (novaInfo == NULL)
-      return 0;
-  if(novo == NULL)
-      return 0;
+    return 0;
+  if (novo == NULL)
+    return 0;
   novo->info = novaInfo;
   novo->proximo = (*novaInfo);
   *novaInfo = novo;
@@ -118,20 +124,26 @@ int insere_lista_final(struct DLista *lista, void *novaInfo)
 {
   struct Noh *novo;
   struct Noh *aux;
-  novo = (struct Noh *) malloc(sizeof(struct Noh));
-  aux = (struct Noh *) malloc(sizeof(struct Noh));
+  novo = (struct Noh *)malloc(sizeof(struct Noh));
+  aux = (struct Noh *)malloc(sizeof(struct Noh));
+
   if (novaInfo == NULL)
-      return 0;
-  if(novo == NULL)
-      return 0;
+    return 0;
+
+  if (novo == NULL)
+    return 0;
+
   novo->info = novaInfo;
   novo->proximo = NULL;
-  if((*novaInfo) == NULL) 
+  
+  if ((*novaInfo) == NULL)
   {
     *novaInfo = novo;
-  } else {
+  }
+  else
+  {
     aux = *novaInfo;
-    while(aux->proximo != NULL) 
+    while (aux->proximo != NULL)
     {
       aux = aux->proximo;
     }
@@ -142,30 +154,30 @@ int insere_lista_final(struct DLista *lista, void *novaInfo)
 
 void mostrarLista(struct DLista *lista, funcaoImpressao fi)
 {
-   struct Noh *atual;
-   atual = lista->primeiro;
+  struct Noh *atual;
+  atual = lista->primeiro;
 
-   while (atual != NULL)
-   {
-        fi(atual->info);
-        atual = atual->proximo;
-   }
+  while (atual != NULL)
+  {
+    fi(atual->info);
+    atual = atual->proximo;
+  }
 }
 
 struct DLista *controiLista(void *vet, int tam)
 {
-   struct DLista *novaLista; 
-   novaLista = criarDescritor();
-   struct Noh *p; 
-   int i;
-   novaLista = NULL;
+  struct DLista *novaLista;
+  novaLista = criarDescritor();
+  struct Noh *p;
+  int i;
+  novaLista = NULL;
 
-   for(i = n - 1; i >= 0; i--)
-   {
-      p = (struct Noh *) malloc(sizeof(struct Noh));
-      p->proximo = novaLista;
-      p->info = v[i];
-      novaLista = p;
-   }
-   return novaLista;
+  for (i = n - 1; i >= 0; i--)
+  {
+    p = (struct Noh *)malloc(sizeof(struct Noh));
+    p->proximo = novaLista;
+    p->info = v[i];
+    novaLista = p;
+  }
+  return novaLista;
 }
