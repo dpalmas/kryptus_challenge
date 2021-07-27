5d7b65bbc6960d2e632f13aa8a9c513446d1ed00
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-07-27 19:23:20 -0300

    Update kryptus_challenge2.c

--- kryptus_challenge2.c
+++ kryptus_challenge2.c
@@ -10,63 +10,66 @@
 
 #include <stdio.h>
 
-typedef void (*funcaoImpressao)  (void *inf);
-typedef  int (*funcaoComparacao) (void *, void *);
+typedef void (*funcaoImpressao)(void *inf);
+typedef int (*funcaoComparacao)(void *, void *);
 
 struct Noh
 {
-    struct Noh *anterior;	
-    void *info;
-    struct Noh *proximo;
+  struct Noh *anterior;
+  void *info;
+  struct Noh *proximo;
 };
 
 struct DLista
 {
-   struct Noh *primeiro;
-   struct Noh *ultimo;
-   int tamanho;
-   funcaoComparacao fc;
+  struct Noh *primeiro;
+  struct Noh *ultimo;
+  int tamanho;
+  funcaoComparacao fc;
 };
 
-struct DLista* criarLista(funcaoComparacao pfc)
+struct DLista *criarLista(funcaoComparacao pfc)
 {
   struct DLista *descritor;
 
-  descritor = (struct DLista *) malloc(sizeof(struct DLista));
-  descritor->primeiro   = NULL;
-  descritor->ultimo     = NULL;
+  descritor = (struct DLista *)malloc(sizeof(struct DLista));
+  descritor->primeiro = NULL;
+  descritor->ultimo = NULL;
   descritor->tamanho = 0;
-  descritor->fc         = pfc;
+  descritor->fc = pfc;
 
   return descritor;
 }
 
 int vaziaListaDinamica(struct DLista *lista)
 {
-   return(lista->tamanho == 0);
+  return (lista->tamanho == 0);
 }
 
 void insereOrdenado(struct DLista *lista, void *valor)
 {
-   struct Noh *novo;
-   struct Noh *anterior = NULL; 
-   struct Noh p = lista;
-   
-   while(p != NULL && p->info < valor)
-   {
-      anterior = p;
-      p = p->proximo;
-   }
+  struct Noh *novo;
+  struct Noh *anterior = NULL;
+  struct Noh p = lista;
 
-   novo = (struct Noh *) malloc(sizeof(struct Noh));
-   novo->info = valor;
+  while (p != NULL && p->info < valor)
+  {
+    anterior = p;
+    p = p->proximo;
+  }
 
-   if (anterior == NULL)
-   {
-      novo->proximo = lista;
-      lista = novo;
-   } else {  
-      novo->proximo = anterior->proximo;
-      anteriro->proximo = novo;
-   return lista;   
+  novo = (struct Noh *)malloc(sizeof(struct Noh));
+  novo->info = valor;
+
+  if (anterior == NULL)
+  {
+    novo->proximo = lista;
+    lista = novo;
+  }
+  else
+  {
+    novo->proximo = anterior->proximo;
+    anteriro->proximo = novo;
+    return lista;
+  }
 }
